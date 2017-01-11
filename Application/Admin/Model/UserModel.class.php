84cf930b058bbf2acfab93dd1d1883da9197d9ac
levy515 <522311670@qq.com>
2017-01-11 17:38:29 +0800

    xinlong

---
+++ Application/Admin/Model/UserModel.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Admin\Model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: liwei
+ * Date: 2017/1/4
+ * Time: 22:19
+ */
+
+use Think\Model;
+
+class UserModel
+{
+
+    public function User()
+    {
+        echo "fuck";
+    }
+}
+
+
+
+?>
